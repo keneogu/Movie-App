edc775a8b0c6e66d8cce3251ee0542f6591193a2
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-01-05 12:53:35 +0100

    Add Routes to different pages in the App.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1>new react app</h1>
+    <div className="app">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:imdbID" element={<MovieDetail />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
