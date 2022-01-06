a48368780aca296e0073df1e47358e4206f609ee
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-01-06 11:15:46 +0100

    fetch Api from omdb website into the home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import MovieList from '../MovieList/MovieList';
+import movieApi from "../../common/apis/movieApi";
+import { APIKey } from "../../common/apis/movieApiKey";
 
 const Home = () => {
+	useEffect(() => {
+		const movieText = "Harry";
+	  const fetchMovies = async () => {
+		const response = await movieApi
+			.get(`?apikey=${APIKey}&s=${movieText}&type=movie`
+		)
+			.catch((err) => {
+				console.log("Err :", err);
+			});
+			console.log("THE response from API", response);
+		};
+		fetchMovies();
+	}, []);
+	
 	return (
-		<div>
-			Home
+		<div className="banner-img">
+			<MovieList />
 		</div>
 	);
 };
