ba7f89fdb833b16ea44c8cf8fca87536d976d43c
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-01-07 10:05:38 +0100

    Add useDispatch in the Home page to update the modified state

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,22 +1,14 @@
 import React, { useEffect } from 'react';
 import MovieList from '../MovieList/MovieList';
-import movieApi from "../../common/apis/movieApi";
-import { APIKey } from "../../common/apis/movieApiKey";
+import { useDispatch } from "react-redux";
+import { fetchAsyncMovies, fetchAsyncShows } from '../../features/movies/movieSlice';
 
 const Home = () => {
+	const dispatch = useDispatch();
 	useEffect(() => {
-		const movieText = "Harry";
-	  const fetchMovies = async () => {
-		const response = await movieApi
-			.get(`?apikey=${APIKey}&s=${movieText}&type=movie`
-		)
-			.catch((err) => {
-				console.log("Err :", err);
-			});
-			console.log("THE response from API", response);
-		};
-		fetchMovies();
-	}, []);
+		dispatch(fetchAsyncMovies());
+		dispatch(fetchAsyncShows());
+	}, [dispatch]);
 	
 	return (
 		<div className="banner-img">
