501d0c724617fc65b8e632126c9613d24fd8d4d0
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-01-07 10:02:04 +0100

    Add movie list to display on the home page

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import { useSelector } from "react-redux";
+import { getAllMovies, getAllShows } from "../../features/movies/movieSlice";
+import MovieCard from "../MovieCard/MovieCard";
+import "./MovieList.scss";
 
 const MovieList = () => {
+	const movies = useSelector(getAllMovies);
+	const shows = useSelector(getAllShows);
+	let renderMovies, renderShows = "";
+
+	renderMovies = 
+	movies.Response === "True" ? (
+		movies.Search.map((movie, index) => (
+			<MovieCard Key={index} data={movie} />
+		))
+	) : (
+		<div className="movie-error">
+			<h3>{movies.Error}</h3>
+		</div>
+	);
+
+	renderShows = 
+	shows.Response === "True" ? (
+		shows.Search.map((movie, index) => (
+			<MovieCard Key={index} data={movie} />
+		))
+	) : (
+		<div className="shows-error">
+			<h3>{shows.Error}</h3>
+		</div>
+	);
 	return (
-		<div>
-			MovieList
+		<div className="movie-wrapper">
+			<div className="movie-list">
+				<h2>Movies</h2>
+				<div className="movie-container">
+					{renderMovies}
+				</div>
+			</div>
+			<div className="show-list">
+				<h2>Shows</h2>
+				<div className="movie-container">
+					{renderShows}
+				</div>
+			</div>
 		</div>
 	);
 };
