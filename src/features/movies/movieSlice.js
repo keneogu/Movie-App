d250a7d373fa352158922d737f888746f2da24b4
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-01-08 13:23:25 +0100

    Update the fetch API to get the searched movie

--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -3,18 +3,16 @@ import movieApi from "../../common/apis/movieApi";
 import { APIKey } from "../../common/apis/movieApiKey";
 
 export const fetchAsyncMovies = createAsyncThunk("movies/fetchAsyncMovies",
- async () => {
-	const movieText = "Harry";
+ async (term) => {
 	const response = await movieApi
-			.get(`?apikey=${APIKey}&s=${movieText}&type=movie`)
+			.get(`?apikey=${APIKey}&s=${term}&type=movie`)
 			return  response.data;
 });
 
 export const fetchAsyncShows = createAsyncThunk("movies/fetchAsyncShows",
- async () => {
-	const seriesText = "Friends";
+ async (term) => {
 	const response = await movieApi
-			.get(`?apikey=${APIKey}&s=${seriesText}&type=series`)
+			.get(`?apikey=${APIKey}&s=${term}&type=series`)
 			return  response.data;
 });
 
